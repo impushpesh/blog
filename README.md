c48ec0d0461ecdfdc4e328599c5dbbb0188fbf9d
Pushpesh <ppant9418@gmail.com>
2024-07-24 17:45:33 +0530

    Updated

--- README.md
+++
@@ -1,2 +0,0 @@
-# blog
-A simple blog app.
--- server/routes/main.js
+++
@@ -1,8 +0,0 @@
-const express= require('express');
-const router= express.Router();
-
-//Routes
-router.get('/',(req,res)=>{
-    res.send('hello')
-})
-module.exports=router;
