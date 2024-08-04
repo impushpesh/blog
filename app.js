ea70a145b1ed470120cb4fcd1ad9cec343230edb
Pushpesh <ppant9418@gmail.com>
2024-08-04 19:20:38 +0530

    Updated visuals and added like feature

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const userModel = require('./models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+
+const userModel = require('./models/user'); //Include usermodel
+const postModel = require('./models/post'); //Include blog
 
 const app = express();
 
@@ -62,29 +64,91 @@ app.post('/login', async (req, res) => {
     });
 });
 
+app.get('/logout', (req, res)=>{
+    res.cookie('token', '');
+    res.redirect('/login')
+});
+
 // Middleware for protected routes
-function isLoggedIn(req, res, next) {
+async function isLoggedIn(req, res, next) {
     const token = req.cookies.token;
     if (!token) {
-        return res.send("Please login first");
+        return res.redirect("/login");
     }
     try {
         const data = jwt.verify(token, secretKey);
-        req.user = data;
+        const user = await userModel.findOne({ email: data.email });
+        if (!user) {
+            return res.send("User not found, please login again");
+        }
+        req.user = user;
         next();
     } catch (error) {
         return res.send("Invalid token, please login again");
     }
 }
 
-app.get('/logout', (req, res) => {
-    res.cookie('token', '', { expires: new Date(0) });
-    res.redirect('/');
+app.get('/profile', isLoggedIn, async (req, res) => {
+    let user= await userModel.findOne({email: req.user.email}).populate('posts')
+    res.render('profile', {user});
 });
 
-app.get('/profile', isLoggedIn, (req, res) => {
-    res.render('index', { user: req.user });
+app.get('/like/:id', isLoggedIn, async (req, res) => {
+    let post= await postModel.findOne({_id: req.params.id}).populate('user')
+    
+    
+    if(post.likes.indexOf(req.user._id)=== -1){
+        post.likes.push(req.user.id);
+    }
+    else{
+        post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+    }
+
+    post.save();
+    res.redirect('/profile')
 });
+
+app.get('/edit/:id', isLoggedIn, async (req, res) => {
+    let post= await postModel.findOne({_id: req.params.id}).populate('user')
+    res.render('edit',{post})
+});
+
+app.post('/update/:id', isLoggedIn, async (req, res) => {
+    let post= await postModel.findOneAndUpdate({_id: req.params.id}, {content: req.body.content})
+    res.redirect('/profile');
+});
+
+
+app.post('/post', isLoggedIn, async (req, res) => {
+    let user= await userModel.findOne({email: req.user.email})
+    let {content} = req.body;
+    let post = await postModel.create({
+        user: user._id,
+        content
+    });
+    user.posts.push(post._id);
+    await user.save(); 
+    res.redirect('/profile');
+});
+
+app.get('/feed', isLoggedIn, async (req, res) => {
+    let posts = await postModel.find({}).populate('user');
+    res.render('feed', { posts: posts, user: req.user });
+});
+
+app.get('/like-feed/:id', isLoggedIn, async (req, res) => {
+    let post = await postModel.findOne({ _id: req.params.id }).populate('user');
+
+    if (post.likes.indexOf(req.user._id) === -1) {
+        post.likes.push(req.user._id);
+    } else {
+        post.likes.splice(post.likes.indexOf(req.user._id), 1);
+    }
+
+    await post.save();
+    res.redirect('/feed');
+});
+
 
 app.listen(3000, function () {
     console.log("Server is running on port 3000");
--- models/user.js
+++ models/user.js
@@ -2,10 +2,23 @@ const mongoose= require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/blog')
 
-const userschema= mongoose.Schema({
-    userName: String,
-    email: String,
-    password: String
+const userSchema = mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true  
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'blog' }]  
 });
 
-module.exports= mongoose.model('user', userschema);
+
+module.exports= mongoose.model('user', userSchema);
